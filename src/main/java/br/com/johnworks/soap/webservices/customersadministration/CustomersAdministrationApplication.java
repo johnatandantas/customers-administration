04169f2e784112e57a0e7a7b097600f46d652f12
Johnatan Dantas <dantas.johnatan@gmail.com>
2020-06-20 09:44:31 -0300

    first commit

---
+++ src/main/java/br/com/johnworks/soap/webservices/customersadministration/CustomersAdministrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.johnworks.soap.webservices.customersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johnworks/soap/webservices/customersadministration/CustomersAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johnworks.soap.webservices.customersadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
